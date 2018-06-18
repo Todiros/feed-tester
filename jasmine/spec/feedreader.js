9db4448edc2fd539e25a0a90cf17d855e627ce7c
Tony Borisov <tony.d.borisov@gmail.com>
2018-06-19 01:15:22 +0300

    test: create test suite (Initial Entries)

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -74,8 +74,8 @@ $(function() {
        });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
-
+    /* Test suite named "Initial Entries" */
+    describe('Initial Entries', () => {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
@@ -89,4 +89,5 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });
 }());
